5afc0101d69163b854440b579f4ec1609c5e084d
Valentina <valen231120gmail.com>
2025-08-19 16:06:46 -0500

    declaración de variables y tipo de dato String

---
+++ 01-variables.js
@@ -0,0 +1,51 @@
+// alert("Hello world")
+// ---------------------------------
+// CONVENSION NOMBRES PARA CODIFICAR
+// ---------------------------------
+
+// UpperCamelCase  => En JS se usa para nombrar las clases 
+// lowerCamelCase  => En JS se usa para nombrar las variables
+// SCREAMING_SNAKE_CASE => En JS se usa para nombrar las constantes
+
+// snake_case      => En JS generealmente NO se usa, esta convencion es muy usada en el lenguaje Python
+
+// caja-de-kebac   => Se usa para nombrar las imágenes, los repositorios
+
+// Caja-De-Tren    => 
+
+// Variables
+// Ya no se declara una variable con var 
+// EN JS NO es obligatorio terminar una sentencia con ;
+// EN JS NO es obligatorio indicar el tipo de variable
+
+var product = "sensor-1";
+
+// No se puede iniciar una variable con - al medio
+//var -sensor = "sensor-2";
+
+// No se puede iniciar una variable con número
+//var 1sensor = "sensor-3";
+
+// Si es permitido iniciar una variable con _ guion al piso
+var _sensor = "sensor-4";
+
+// Crear una variable con las 3 formas
+var sensor = "Sensor de lluvia";
+var sensor = "Sensor de temperatura";
+var sensor = "Sensor de presión";
+
+console.log(product);
+
+let UserName = "Valen";
+// let UserName = "Tina";
+console.log(UserName);
+
+// Ejemplo con const
+// No puedo cambiarle el valor a una variable constante
+const maxSize = 1024;
+// maxSize = 124568;
+
+// Otro ejemplo con var
+console.log(userName2)
+
+var userName2 = "Tina";
---
+++ 02-tipo-dato-string.js
@@ -0,0 +1,21 @@
+// String o cadena de texto
+const sensor = "temperatura";
+const sensor2 = "ph";
+
+// Dos formas no tan comunes
+const sensor3 = String('Presión');
+const sensor4 = new String('Presión');
+
+console.log(sensor);
+console.log(sensor3);
+
+// con typeof se puede ver el tipo de dato
+console.log(typeof sensor2);
+console.log(sensor2);
+
+
+let product = "Televisor de 40\"";
+let product2 = "Televisor de 32\"";
+
+console.log(product);
+console.log(product2);
